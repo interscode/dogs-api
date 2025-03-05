e6212d4b3ae471b3617fd947662410816e19a34a
mbalderas08@alumnos.uaq.mx <mbalderas08@alumnos.uaq.mx>
2025-03-05 15:58:38 -0600

    feat: add dogs hook

---
+++ hooks/useDogs.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export function useDogs() {
+    const [dogs, setDogs] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    async function getDogsByBreed(breed) {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/5`);
+            const data = await response.json();
+            setDogs(data.message);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    async function getDogsRandom() {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch(`https://dog.ceo/api/breeds/image/random/5`);
+            const data = await response.json();
+            setDogs(data.message);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return { dogs, loading, error, getDogsByBreed, getDogsRandom };
+}
