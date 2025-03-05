48b3b41a6653d28891f583c1037a5a6de4144df6
GaelRS <grosas04@alumnos.uaq.mx>
2025-03-05 16:41:42 -0600

    feat: Se añadio el boton de random

--- components/main.js
+++ components/main.js
@@ -1,37 +1,41 @@
-import { View } from "react-native";
+import { Pressable, View, Text } from "react-native";
 import { styles } from "../styles/styles";
 import DynamicList from "./dynamic-list";
 import { useState } from "react";
-import { data } from "../data";
 import { Header } from "./header";
 import { Search } from "./search";
+import { useDogs } from "../hooks/useDogs";
 
 export default function Main() {
   const [theme, setTheme] = useState("light");
-  const [blocks, setBlocks] = useState(data);
+  const {getDogsRandom, dogs} = useDogs();
+
+  const handleRandom = () => {
+    getDogsRandom();
+  };
 
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
-  const filterBlocks = (text) => {
-    const filteredBlocks = data.filter((block) =>
-      block.name.toLowerCase().includes(text.toLowerCase())
-    );
-    setBlocks(filteredBlocks);
-  };
 
   return (
     <View>
       <Header theme={theme} toggleTheme={toggleTheme} />
-      <Search theme={theme} filterBlocks={filterBlocks} />
+      <Search theme={theme} filterBlocks={() => {}} />
+      <View style={[styles.bottomContainer, { backgroundColor: theme === "light" ? "#f8f8ff" : "#000" }]}>
+        <Pressable onPress={handleRandom} style={styles.buttonRandom}>
+          <Text style={styles.buttonText}>Random</Text>
+        </Pressable>
+      </View>
+      
       <View
         style={[
           styles.body,
           { backgroundColor: theme === "light" ? "#f8f8ff" : "#000" },
         ]}
       >
-        <DynamicList theme={theme} data={blocks} />
+        <DynamicList theme={theme} data={dogs} />
       </View>
     </View>
   );
